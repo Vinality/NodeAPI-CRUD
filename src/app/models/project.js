ecc0e2ee11062538ad45bc4b085fc0c28c893e77
Vinícius Crepschi <vibracre@gmail.com>
2019-02-15 19:05:37 -0200

    Completed API

---
+++ src/app/models/project.js
@@ -0,0 +1,34 @@
+const mongoose = require('../../database');
+mongoose.set('useFindAndModify', false);
+
+const ProjectSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+
+	description: {
+		type: String,
+		required: true
+	},
+
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+
+	tasks: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Task'
+	}],
+
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = Project;
---
+++ src/modules/mailer.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
+const { host, port, user, pass } = require('../config/mail.json')
+const path = require('path')
+
+const transport = nodemailer.createTransport({
+  host,
+  port,
+  auth: { user, pass }
+})
+
+transport.use('compile', hbs({
+  viewEngine: 'handlebars',
+  viewPath: path.resolve('./src/resources/email/'),
+  extName: '.html'
+}))
+
+module.exports = transport
